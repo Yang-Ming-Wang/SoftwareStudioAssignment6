1542281afd1fe5ee1b931d35cbc577978fdd1bba
Yang-Ming-Wang <r910214@yahoo.com.tw>
2016-05-08 13:34:25 +0800

    Finish the basic input and show the circle

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -9,15 +9,23 @@ import processing.core.PApplet;
 public class Character {
 	
 	private MainApplet parent;
-
-	public Character(MainApplet parent){
+	private String name;
+	private int value;
+	private int colour;
+	private int x,y;
+	public Character(MainApplet parent,String name,int value,int colour,int x,int y){
 
 		this.parent = parent;
-		
+		this.name = name;
+		this.value = value;
+		this.colour = colour;
+		this.x = x;
+		this.y = y;
 	}
 
 	public void display(){
-
+		parent.fill(colour);
+		parent.arc(x, y, 30, 30, 0f, 2*3.14f);
 	}
 	
 }
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -1,6 +1,10 @@
 package main.java;
 
 import processing.core.PApplet;
+import java.lang.StringBuilder;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
+import java.util.ArrayList;
 
 /**
 * This class is for sketching outcome using Processing
@@ -10,7 +14,10 @@ import processing.core.PApplet;
 public class MainApplet extends PApplet{
 	private String path = "main/resources/";
 	private String file = "starwars-episode-1-interactions.json";
-	
+	private JSONObject data;
+	private JSONArray nodes, links;
+	private char episode = '1';
+	private ArrayList<Character> characters = new ArrayList<Character>();
 	private final static int width = 1200, height = 650;
 	
 	public void setup() {
@@ -22,11 +29,36 @@ public class MainApplet extends PApplet{
 	}
 
 	public void draw() {
-
+		for(Character ch: characters)
+			ch.display();
 	}
 
 	private void loadData(){
-
+		StringBuilder temp = new StringBuilder(file);
+		//revise episode here in order to get other JSONfile
+		temp.setCharAt(17, episode);
+		
+		data = loadJSONObject(path + temp);
+		nodes = data.getJSONArray("nodes");
+		links = data.getJSONArray("links");
+		
+		int i;
+		String char_name;
+		int char_value;
+		int char_colour;
+		int char_x;
+		int char_y;
+		
+		for (i = 0;i < nodes.size();i++){
+			char_name = nodes.getJSONObject(i).getString("name");
+			char_value = nodes.getJSONObject(i).getInt("value");
+			char_colour = unhex(nodes.getJSONObject(i).getString("colour").substring(1, 9));
+			char_x = 50+(i/10)*50;
+			char_y = (i%10)*50+50;
+			characters.add(new Character(this,char_name,char_value,char_colour,char_x,char_y));
+		}
+		
+		
 	}
 
 }
