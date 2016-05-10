dcc6af5c266430f35a4f72e6af1047eae8ef5157
Yang-Ming-Wang <r910214@yahoo.com.tw>
2016-05-10 20:54:00 +0800

    Fix the update wrong file for previous commit

--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -17,7 +17,7 @@ import controlP5.ControlP5;
 
 /**
 * This class is for sketching outcome using Processing
-* You can do major UI control and some visualization in this class.  
+* You can do major UI control and some visualization in this class.
 */
 @SuppressWarnings("serial")
 public class MainApplet extends PApplet{
@@ -31,34 +31,34 @@ public class MainApplet extends PApplet{
 	private int noOfselect = 0;
 	private ControlP5 cp5;
 	private final static int width = 1200, height = 650;
-	
+
 	public void setup() {
 
 		size(width, height);
-		
-		cp5 = new ControlP5(this); 
+
+		cp5 = new ControlP5(this);
 		cp5.addButton("ClearBtn")
-		.setLabel("Clear").setPosition(960, 100).setSize(200, 50); 
-		
+		.setLabel("Clear").setPosition(960, 100).setSize(200, 50);
+
 		cp5.addButton("AddAll")
-		.setLabel("AddAll").setPosition(960, 30).setSize(200, 50); 
-		
+		.setLabel("AddAll").setPosition(960, 30).setSize(200, 50);
+
 		PFont pfont = createFont("Arial",20,true); // use true/false for smooth/no-smooth
 		ControlFont cfont = new ControlFont(pfont,40);
-		
-		
+
+
 		cp5.getController("ClearBtn").getCaptionLabel().setFont(cfont);
 		cp5.getController("ClearBtn").setColorBackground(color(35,180,75));
 
 		PFont addfont = createFont("Arial",20,true);
 		ControlFont addAllfont = new ControlFont(pfont,40);
-		
+
 		cp5.getController("AddAll").getCaptionLabel().setFont(addAllfont);
 		cp5.getController("AddAll").setColorBackground(color(35,80,175));
-		
+
 		smooth();
 		loadData();
-		
+
 	}
 	public void AddAll(){
 		for(Character ch: characters){
@@ -67,22 +67,22 @@ public class MainApplet extends PApplet{
 				while(true){
 					Random randx = new Random();
 					Random randy = new Random();
-					
+
 					cx = randx.nextInt(950) + 450;
 					cy = randy.nextInt(570) + 80;
-					
+
 					int squ_cx = (cx - 700 )*(cx - 700);
 					int squ_cy = (cy - 320 )*(cy - 320);
 					if( (squ_cx + squ_cy) == 62500 )
 						break;
 				}
-				
+
 				ch.x = cx;
 				ch.y = cy;
-				
+
 				ch.onCircle = true;
 			}
-			
+
 		}
 	}
 	public void ClearBtn(){
@@ -98,7 +98,7 @@ public class MainApplet extends PApplet{
 			network.clear();
 			loadData();
 		}
-		
+
 		for(Character ch: characters){
 			if(ch.onCircle == true){
 				ch.onCircle = false;
@@ -115,14 +115,14 @@ public class MainApplet extends PApplet{
 			if(source.onCircle && target.onCircle){
 				stroke(0);
 				strokeWeight(net.value);
-				//line(source.x,source.y,target.x,target.y);
-				arc(source.x, source.y, target.x, target.y, 0, 90);
+				line(source.x,source.y,target.x,target.y);
+				//arc(source.x, source.y, target.x, target.y, 0, 90);
 			}
 		}
 		noFill();
 		stroke(0,0,0);
 		arc(700, 320, 500, 500, 0f, 2*3.14f);
-		
+
 		textSize(36);
 		fill(185,120,90);
 		text("Star Wars " + episode,350,0,200,200);
@@ -132,11 +132,11 @@ public class MainApplet extends PApplet{
 		StringBuilder temp = new StringBuilder(file);
 		//revise episode here in order to get other JSONfile
 		temp.setCharAt(17, episode);
-		
+
 		data = loadJSONObject(path + temp);
 		nodes = data.getJSONArray("nodes");
 		links = data.getJSONArray("links");
-		
+
 		int i;
 		String char_name;
 		int char_value;
@@ -151,7 +151,7 @@ public class MainApplet extends PApplet{
 			char_y = (i%10)*60+50;
 			characters.add(new Character(this,char_name,char_value,char_colour,char_x,char_y));
 		}
-		
+
 		int source;
 		int target;
 		int value;
@@ -182,7 +182,7 @@ public class MainApplet extends PApplet{
 		noOfselect = 0;
 		for(Character ch: characters){
 			if (ch.isSelect){
-				
+
 				int square_x = (700 - ch.x)*(700 - ch.x);
 				int square_y = (320 - ch.y)*(320 - ch.y);
 				if( (square_x + square_y)<= 62500 && (square_x + square_y) >= 0){
@@ -190,22 +190,22 @@ public class MainApplet extends PApplet{
 					while(true){
 						Random randx = new Random();
 						Random randy = new Random();
-						
+
 						cx = randx.nextInt(950) + 450;
 						cy = randy.nextInt(570) + 80;
-						
+
 						int squ_cx = (cx - 700 )*(cx - 700);
 						int squ_cy = (cy - 320 )*(cy - 320);
 						if( (squ_cx + squ_cy) == 62500 )
 							break;
 					}
-				
-					
+
+
 					ch.x = cx;
 					ch.y = cy;
-					
+
 					ch.onCircle = true;
-					
+
 				}else{
 					ch.resetPos();
 				}
